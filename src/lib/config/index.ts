13f42cecc9271af77df11ef1052fde0dc073c48b
Your Name <you@example.com>
2021-11-24 00:20:25 +0000

    Update embr urls

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,5 +1,6 @@
 import rinkeby from './rinkeby.json';
 import fantom from './fantom.json';
+import fuji from './fuji.json';
 
 export interface Config {
   key: string;
@@ -82,7 +83,8 @@ const config: Record<string, Config> = {
   //'1': homestead,
   //'42': kovan,
   '4': rinkeby,
-  '250': fantom
+  '250': fantom,
+  '43113': fuji,
   //'137': polygon,
   //'12345': test,
   //'43113': fuji,
